f5b97f85b39518d9c107f19084bbf9c345bd09bf
tua1-ua <124907896+tua1-ua@users.noreply.github.com>
2023-12-23 16:49:29 +0100

    Casi finalizado (falta 2E)

--- Taha_Uzair_Ali_Anjum.py
+++ Taha_Uzair_Ali_Anjum.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.decomposition import PCA
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -9,6 +10,13 @@ from keras.datasets import mnist
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 
 #########################################################################
@@ -44,7 +52,7 @@ def load_MNIST_for_adaboost():
     Y_train = Y_train.astype("int8")
     Y_test = Y_test.astype("int8")
 
-    # return X_train, Y_train, X_test, Y_test
+    return X_train, Y_train, X_test, Y_test
 
 
 #########################################################################
@@ -171,94 +179,6 @@ class AdaboostMulticlase:
 
 
 #########################################################################
-# La clase AdaboostMulticlaseMejorado (multiclasificador fuerte-óptimo)
-#########################################################################
-class DecisionStumps:
-    def __init__(self):
-        self.feature_index = None
-        self.threshold = None
-        self.alpha = None
-        self.polarity = None
-
-    def fit(self, X, y, weights):
-        m, n = X.shape
-        best_error = float('inf')
-
-        for feature_index in range(n):
-            unique_values = np.unique(X[:, feature_index])
-            thresholds = (unique_values[:-1] + unique_values[1:]) / 2.0
-
-            for threshold in thresholds:
-                for polarity in [1, -1]:
-                    predictions = np.ones(m)
-                    predictions[polarity * X[:, feature_index] <= polarity * threshold] = -1
-
-                    error = np.sum(weights * (predictions != y))
-
-                    if error < best_error:
-                        best_error = error
-                        self.feature_index = feature_index
-                        self.threshold = threshold
-                        self.polarity = polarity
-
-        # Calcular el coeficiente alpha
-        self.alpha = 0.5 * np.log((1 - best_error) / (best_error + 1e-10))
-
-    def predict(self, X):
-        m, _ = X.shape
-        predictions = np.ones(m)
-        predictions[self.polarity * X[:, self.feature_index] <= self.polarity * self.threshold] = -1
-        return predictions 
-class AdaboostMulticlaseMejorado:
-    def __init__(self, digitos=10, T=5, A=20, weak_classifier=DecisionStumps):
-        self.digitos = digitos
-        self.T = T
-        self.A = A
-        self.weak_classifiers = [weak_classifier() for _ in range(digitos)]
-
-    def fit(self, X, Y):
-        m, n = X.shape
-        weights = np.ones(m) / m
-        self.classifiers = []
-
-        for t in range(self.T):
-            classifiers = []
-            for class_index in range(self.digitos):
-                classifier = self.weak_classifiers[class_index]
-                classifier.fit(X, self._create_binary_labels(Y, class_index), weights)
-                classifiers.append(classifier)
-
-            errors = np.array([np.sum(weights * (classifier.predict(X) != self._create_binary_labels(Y, class_index)))
-                               for classifier, class_index in zip(classifiers, range(self.digitos))])
-
-            classifier_index = np.argmin(errors)
-            error = errors[classifier_index]
-
-            if error == 0:
-                break
-
-            alpha = 0.5 * np.log((1 - error) / (error + 1e-10))
-
-            for i, classifier in enumerate(classifiers):
-                weights *= np.exp(-alpha * self._create_binary_labels(Y, i) * classifier.predict(X))
-
-            weights /= np.sum(weights)
-
-            self.classifiers.append((class_index, alpha, classifiers[classifier_index]))
-
-    def predict(self, X):
-        votes = np.zeros((self.digitos, X.shape[0]))
-
-        for class_index, alpha, classifier in self.classifiers:
-            votes[class_index, :] += alpha * classifier.predict(X)
-
-        return np.argmax(votes, axis=0)
-
-    def _create_binary_labels(self, Y, class_index):
-        return np.where(Y == class_index, 1, -1)
-
-
-#########################################################################
 # Método para probar el rendimiento del Adaboost (tarea 1A y 1B)
 #########################################################################  
 def tareas_1A_y_1B_adaboost_binario(clase, T, A, verbose=False):
@@ -362,10 +282,6 @@ def tarea_1C_graficas_rendimiento():
 
     # Mostrar el valor de T y A correspondiente a la mejor puntuación
     best_combination = find_best_combination(combinaciones_validas, get_score_fn=lambda T, A: calculate_score(accuracies[-1], times[-1]))
-    #print(f"Mejor puntuación para: {best_combination}")
-
-    # Mostrar el valor de T y A correspondiente al menor tiempo de ejecución
-    #print(f"Mejor tiempo de ejecución para: {combinaciones_validas[min_time_index]}")
 
 
     # Mostrar la tasa de acierto y el tiempo de ejecución para la mejor tasa de acierto
@@ -394,7 +310,8 @@ def tareas_1D_adaboost_multiclase(T, A, verbose=False):
     X_train = X_train.reshape((X_train.shape[0], -1)).astype('float32') / 255
     X_test = X_test.reshape((X_test.shape[0], -1)).astype('float32') / 255
 
-    print(f"\nComenzando el entrenamiento del clasificador Adaboost multiclase, T={T}, A={A}...")
+    if verbose:
+        print(f"\nComenzando el entrenamiento del clasificador Adaboost multiclase, T={T}, A={A}...")
     start_time = time.time()
 
     multi_class_clf = AdaboostMulticlase(digitos=10, T=T, A=A, verbose=verbose)
@@ -406,43 +323,22 @@ def tareas_1D_adaboost_multiclase(T, A, verbose=False):
     predictions = multi_class_clf.predict(X_test)
     accuracy = np.mean(predictions == Y_test) * 100
 
-    print(f"Entrenamiento completado. Tasa de acierto: {accuracy:.2f}%. Tiempo total: {elapsed_time:.3f} s.")
+    if verbose:
+        print(f"Entrenamiento completado. Tasa de acierto: {accuracy:.2f}%. Tiempo total: {elapsed_time:.3f} s.")
 
     return accuracy
 
-
-#########################################################################
-# Método que mostrar rendimiento de Adaboost multiclase óptimo (tarea 1D)
-######################################################################### 
-def tarea_1E_adaboost_multiclase_mejorado(T, A):
-    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
-
-    print(f"\nComenzando el entrenamiento del clasificador Adaboost multiclase mejorado, T={T}, A={A}...")
-    start_time = time.time()
-
-    multi_class_clf = AdaboostMulticlaseMejorado(digitos=10, T=T, A=A)
-    multi_class_clf.fit(X_train, Y_train)
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-
-    predictions = multi_class_clf.predict(X_test)
-    accuracy = np.mean(predictions == Y_test) * 100
-
-    print(f"Entrenamiento completado. Tasa de acierto: {accuracy:.2f}%. Tiempo total: {elapsed_time:.3f} s.")
-
-    return accuracy
 
 
 #########################################################################
 # Adaboost multiclase de scikit-learn con los valor por defecto
 ######################################################################### 
-def tarea_2A_AdaBoostClassifier_default():
+def tarea_2A_AdaBoostClassifier_default(n_estimators=50):
     # Cargamos el dataset de mnist
     X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
 
     # Invocar el clasificador de scikit-learn
-    adaboost_classifier = AdaBoostClassifier()
+    adaboost_classifier = AdaBoostClassifier(n_estimators=n_estimators)
 
     # Imprimir mensaje de inicio del algoritmo
     print("Comenzando el AdaBoost multiclase de scikit-learn")
@@ -461,19 +357,30 @@ def tarea_2A_AdaBoostClassifier_default():
     accuracy = accuracy_score(Y_test, predictions)
 
     # Imprimir la tasas de acierto y el tiempo de ejecución
-    print(f'Tasa de acierto del clasificador AdaBoost: {accuracy}, tiempo de ejecución: {t_ejec:.3f} s.\n')
+    print(f'Tasa de acierto del clasificador AdaBoost de {n_estimators} estimadores: {accuracy}, tiempo de ejecución: {t_ejec:.3f} s.\n')
 
     return accuracy, t_ejec
 
 
 #########################################################################
-# Gráficas comparativas de rendimiento entre implementaciones del
-# Adaboost multiclase
+# Función que muestra las gráficas de rendimiento de los clasificadores
+# de la tarea 1D y 2A (haciendo llamadas a funciones correspondientes)
 ######################################################################### 
-def tarea_2B_graficas_rendimiento():
+def tarea_2B_graficas_rendimiento(bool_1D=True, bool_2A=True):
+    if bool_1D:
+        graficas_multiclase_1D()
+    if bool_2A:
+        graficas_multiclase_2A()
+
+
+#########################################################################
+# Esta función grafica la tasa de acierto y el tiempo de ejecución 
+# para cada combinación de T y A del adaboost multiclase de la tarea 1D
+######################################################################### 
+def graficas_multiclase_1D():
     # Estos rangos los podemos ajustar en función de nuestras necesidades
-    T_values = list(range(5, 180, 10))  # Valores de 5 a 180 con incrementos de 10
-    A_values = list(range(5, 30, 5))  # Valores de 5 a 30 con incrementos de 5
+    T_values = list(range(10, 90, 10))  # Valores de 10 a 90 con incrementos de 10
+    A_values = list(range(10, 90, 10))  # Valores de 10 a 90 con incrementos de 10
 
     # Filtrar combinaciones válidas según la restricción T * A ≤ 900
     combinaciones_validas = [(T, A) for T in T_values for A in A_values if T * A <= 900]
@@ -526,15 +433,177 @@ def tarea_2B_graficas_rendimiento():
     # Mostrar un solo print que incluye la información de los tres apartados
     print(accuracy_info)
     plt.show()
+    
+    
+#########################################################################
+# Esta función grafica la tasa de acierto y el tiempo de ejecución 
+# para cada combinación de n_estimators del adaboost multiclase de la  2A
+#########################################################################     
+def graficas_multiclase_2A():
+    n_estimators_values = list(range(10, 100, 10))  # Adjust the range based on your needs
+
+    accuracies = []
+    times = []
+
+    for n_estimators in n_estimators_values:
+        result = tarea_2A_AdaBoostClassifier_default(n_estimators=n_estimators) 
+        accuracies.append(result[0])
+        times.append(result[1])
+
+    # Plotting
+    fig, ax1 = plt.subplots(figsize=(12, 8))
+
+    # Configurar el primer eje Y (tasa de acierto)
+    ax1.set_xlabel('n_estimators')
+    ax1.set_ylabel('Tasa de Acierto', color='tab:blue')
+    ax1.plot(n_estimators_values, accuracies, color='tab:blue', marker='o', label='Tasa de Acierto')
+    ax1.legend(loc='upper left')
+
+    # Configurar el segundo eje Y (tiempo de ejecución)
+    ax2 = ax1.twinx()
+    ax2.set_ylabel('Tiempo de Ejecución (s)', color='tab:red')
+    ax2.plot(n_estimators_values, times, color='tab:red', marker='s', label='Tiempo de Ejecución')
+    ax2.legend(loc='upper right')
+
+    plt.title('Evolución de la Tasa de Acierto y Tiempo de Ejecución con n_estimators')
+    plt.show()
+
+    return accuracies, times
+
+def tarea_2A_AdaBoostClassifier_optimized(n_estimators=50, learning_rate=1.0, reduce_dimensionality=False, pca_components=None, n_jobs=None):
+    # Cargamos el dataset de MNIST
+    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
+
+    # Reducción de dimensionalidad si se especifica
+    if reduce_dimensionality:
+        pca = PCA(n_components=pca_components)
+        X_train = pca.fit_transform(X_train)
+        X_test = pca.transform(X_test)
+
+    # Configuración de AdaBoostClassifier con un clasificador débil personalizado
+    base_classifier = DecisionTreeClassifier(max_depth=5, min_samples_split=2)
+
+    # Configuración del clasificador débil para paralelización
+    base_classifier.n_jobs = n_jobs
+
+    adaboost_classifier = AdaBoostClassifier(base_classifier, n_estimators=n_estimators, learning_rate=learning_rate)
+
+    # Entrenamiento y medición del tiempo de ejecución
+    start = time.time()
+    adaboost_classifier.fit(X_train, Y_train)
+    end = time.time()
+    t_ejec = end - start
+
+    # Evaluación del modelo en el conjunto de prueba
+    predictions = adaboost_classifier.predict(X_test)
+    accuracy = accuracy_score(Y_test, predictions)
+
+    # Imprimir resultados
+    print(f'Tasa de acierto del clasificador AdaBoost con {n_estimators} estimadores y tasa de aprendizaje {learning_rate}: {accuracy:.4f}, tiempo de ejecución: {t_ejec:.3f} s.\n')
 
 
 
+def build_mlp_model(input_shape, num_classes, hidden_layers=1, neurons_per_layer=128, activation='relu', output_activation='softmax', optimizer='adam', learning_rate=0.001):
+    model = Sequential()
+    
+    model.add(Dense(neurons_per_layer, activation=activation, input_shape=(input_shape,)))
+    model.add(BatchNormalization())  # Agregamos normalización por lotes
+
+    for _ in range(hidden_layers - 1):
+        model.add(Dense(neurons_per_layer, activation=activation))
+        model.add(BatchNormalization())
+
+    model.add(Dense(num_classes, activation=output_activation))
+
+    opt = Adam(learning_rate=learning_rate)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
+
+def tarea_2D_AdaBoostClassifier_DecisionTree(hidden_layers=1, neurons_per_layer=128, activation='relu', output_activation='softmax', optimizer='adam', learning_rate=0.001):
+    # Cargar los datos de MNIST
+    X_train, Y_train, X_test, Y_test = load_MNIST_for_adaboost()
+
+    # Convertir las etiquetas a categorías
+    Y_train_cat = to_categorical(Y_train, num_classes=10)
+    Y_test_cat = to_categorical(Y_test, num_classes=10)
+
+    # Dividir datos de entrenamiento para validación
+    X_train, X_val, Y_train_cat, Y_val_cat = train_test_split(X_train, Y_train_cat, test_size=0.2, random_state=42)
+
+    input_shape = X_train.shape[1]
+    num_classes = Y_train_cat.shape[1]
+
+    # Construir el modelo MLP
+    model = build_mlp_model(input_shape, num_classes, hidden_layers, neurons_per_layer, activation, output_activation, optimizer, learning_rate)
+
+    # Entrenar el modelo
+    start_time = time.time()
+    model.fit(X_train, Y_train_cat, epochs=10, batch_size=32, validation_data=(X_val, Y_val_cat), verbose=2)
+    end_time = time.time()
+
+    # Evaluar el modelo
+    test_loss, test_accuracy = model.evaluate(X_test, Y_test_cat, verbose=0)
+    
+    print(f"Exactitud en los datos de prueba: {test_accuracy * 100:.2f}%")
+    print(f"Tiempo de ejecución: {end_time - start_time:.3f} s\n")
+
+    return test_accuracy
+
+
+def tarea_2E_CNN_Keras(n_conv_layers, n_filters, filter_size, n_dense_layers, n_dense_neurons):
+    # Cargar datos de MNIST
+    (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+    # Preprocesamiento de datos
+    X_train = X_train.reshape((X_train.shape[0], 28, 28, 1)).astype('float32') / 255
+    X_test = X_test.reshape((X_test.shape[0], 28, 28, 1)).astype('float32') / 255
+
+    Y_train = to_categorical(Y_train)
+    Y_test = to_categorical(Y_test)
+
+    # Dividir datos de entrenamiento para validación
+    X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.2, random_state=42)
+
+    # Construir el modelo CNN
+    model = Sequential()
+
+    # Capas convolucionales
+    for _ in range(n_conv_layers):
+        model.add(Conv2D(n_filters, (filter_size, filter_size), activation='relu', input_shape=(28, 28, 1)))
+        model.add(MaxPooling2D((2, 2)))
+
+    # Capas totalmente conectadas
+    model.add(Flatten())
+    for _ in range(n_dense_layers):
+        model.add(Dense(n_dense_neurons, activation='relu'))
+
+    model.add(Dense(10, activation='softmax'))  # Capa de salida
+
+    # Compilar el modelo
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    # Entrenar el modelo
+    start_time = time.time()
+    model.fit(X_train, Y_train, epochs=10, batch_size=64, validation_data=(X_val, Y_val), verbose=2)
+    end_time = time.time()
+
+    # Evaluar el modelo
+    test_loss, test_accuracy = model.evaluate(X_test, Y_test, verbose=0)
+
+    print(f"Exactitud en los datos de prueba: {test_accuracy * 100:.2f}%")
+    print(f"Tiempo de ejecución: {end_time - start_time:.3f} s\n")
+
+    return test_accuracy
 
 
 if __name__ == "__main__":
     #rend_1A = tareas_1A_y_1B_adaboost_binario(clase=9, T=45, A=10, verbose=True)
     #tarea_1C_graficas_rendimiento()
-    rend_1D = tareas_1D_adaboost_multiclase(T=100, A=30, verbose=True)
-    #rend_1E = tarea_1E_adaboost_multiclase_mejorado(T=50, A=30)
-    #rend_2A = tarea_2A_AdaBoostClassifier_default()
-    tarea_2B_graficas_rendimiento()
+    #rend_1D = tareas_1D_adaboost_multiclase(T=30, A=30, verbose=True)
+    #rend_2A = tarea_2A_AdaBoostClassifier_default(n_estimators=40) # la configuración óptima
+    #tarea_2B_graficas_rendimiento(bool_1D=False, bool_2A=True)
+    #rend_2B= tarea_2A_AdaBoostClassifier_optimized(n_estimators=40, learning_rate=0.1, reduce_dimensionality=True, pca_components=20, n_jobs=-1)
+    #rend_2D = tarea_2D_AdaBoostClassifier_DecisionTree(hidden_layers=2, neurons_per_layer=256, learning_rate=0.01)
+    rend_2E_CNN = tarea_2E_CNN_Keras(n_conv_layers=2, n_filters=32, filter_size=3, n_dense_layers=2, n_dense_neurons=128)
+
